021d23a4caf9992bec64450f2812bd4ecfbbdd30
paulyan01 <126923309+paulyan01@users.noreply.github.com>
2023-05-03 19:11:34 -0400

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,1 +1,13 @@
-
+provider "aws" {
+  region = "us-east-1"
+  access_key=var.AWS_ACCESS_KEY
+  secret_key=var.AWS_SECRET_KEY
+}
+resource "aws_instance" "web" {
+  ami           = "ami-0889A44B331DB0194"
+  instance_type = "t2.micro"
+  
+  tags = {
+    name = "HelloWorld"
+  }
+}
