3c0e68dd0c7dafa9143660ae51c00aed95bd0eca
paulyan01 <126923309+paulyan01@users.noreply.github.com>
2023-05-03 19:17:32 -0400

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -4,7 +4,7 @@ provider "aws" {
   secret_key=var.AWS_SECRET_KEY
 }
 resource "aws_instance" "web" {
-  ami           = "ami-0889A44B331DB0194"
+  ami           = "ami-0889a44b331db0194"
   instance_type = "t2.micro"
   
   tags = {
